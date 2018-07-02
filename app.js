8a5d3bebe0b5d7e0387c727e5e093b1a57ca84ca
git-SergBond <git-serg.bond@yandex.ru>
2018-07-02 19:09:25 +0300

    add hint on hower, and begin fill properies place marks

--- app.js
+++ app.js
@@ -242,6 +242,7 @@ return responce;
         .search('geometry.type = "Point"')
         .setOptions('preset', 'twirl#blueStretchyIcon');
     },
+    //--------------События для меток--------------------
     click_Placemark: function (event) {
       //при клике на метке, в блоке информации выделяеются даныне и сама метка
       this.cur_point = event.get('target').geometry.getCoordinates();
@@ -272,11 +273,90 @@ return responce;
         }
       }
     },
+    make_service_hint: function () {
+      // Создание макета содержимого хинта.
+      // Макет создается через фабрику макетов с помощью текстового шаблона.
+      let HintLayout = ymaps.templateLayoutFactory.createClass(
+        "<div class='my-hint'>" +
+        "<img src = '{{ properties.imageUrl }}'>"+
+        "<p><b>{{ properties.name }}</b>" +
+        "<br/>{{ properties.address }}" +
+        "<br/>{{ properties.phoneNumber }}" +
+        "<br/><b>{{ properties.price }} Руб.</b>" +
+        "</p></div>", {
+          // Определяем метод getShape, который
+          // будет возвращать размеры макета хинта.
+          // Это необходимо для того, чтобы хинт автоматически
+          // сдвигал позицию при выходе за пределы карты.
+          getShape: function () {
+            var el = this.getElement(),
+              result = null;
+            if (el) {
+              var firstChild = el.firstChild;
+              result = new ymaps.shape.Rectangle(
+                new ymaps.geometry.pixel.Rectangle([
+                  [0, 0],
+                  [firstChild.offsetWidth, firstChild.offsetHeight]
+                ])
+              );
+            }
+            return result;
+          }
+        }
+      );
+      return HintLayout;
+    },
+    make_shares_hint: function () {
+      // Создание макета содержимого хинта.
+      // Макет создается через фабрику макетов с помощью текстового шаблона.
+      let HintLayout = ymaps.templateLayoutFactory.createClass(
+        "<div class='my-hint'>" +
+        "<img src = '{{ properties.imageUrl }}'>"+
+        "<p><b>{{ properties.name }}</b>" +
+        "<br/>{{ properties.address }}" +
+        "<br/>{{ properties.phoneNumber }}" +
+        "<br/><b>АКЦИЯ</b> " +
+        "</p></div>", {
+          // Определяем метод getShape, который
+          // будет возвращать размеры макета хинта.
+          // Это необходимо для того, чтобы хинт автоматически
+          // сдвигал позицию при выходе за пределы карты.
+          getShape: function () {
+            var el = this.getElement(),
+              result = null;
+            if (el) {
+              var firstChild = el.firstChild;
+              result = new ymaps.shape.Rectangle(
+                new ymaps.geometry.pixel.Rectangle([
+                  [0, 0],
+                  [firstChild.offsetWidth, firstChild.offsetHeight]
+                ])
+              );
+            }
+            return result;
+          }
+        }
+      );
+      return HintLayout;
+    },
     //---------------- ОТРИСОВКА ОБЛАСТИ НА КАРТЕ---------
     add_placemarks_on_map: function(arr_placemarks){
       //добавление меток на карту и информации о них
+      let HintShare = this.make_service_hint();
+      let HintServ = this.make_shares_hint();
       arr_placemarks.forEach(placemark => {
-        let p = new ymaps.Placemark(placemark.coords);
+        let p = new ymaps.Placemark(placemark.coords, {}, {
+          hintLayout: (placemark.price == null || placemark.price == undefined) ? HintServ : HintShare
+        });
+        p.properties.set({
+          name: placemark.name,
+          imageUrl: placemark.imageUrl,
+          address: placemark.address,
+          phoneNumber: placemark.phoneNumber,
+          countReviews: placemark.countReviews,
+          price: placemark.price,
+          stars: placemark.stars
+        });
         p.events.add('click', this.click_Placemark);
         this.mapInstanse.geoObjects.add(p);
       });
