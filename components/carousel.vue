a94e0fc4039909bd57fadcbf63eaec98a76cd536
git-SergBond <git-serg.bond@yandex.ru>
2018-07-05 21:35:54 +0300

    redizain courusel

--- components/carousel.vue
+++ components/carousel.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="carousel">
-    <button class="btn" @click="before_image"><</button>
-
     <img 
     v-for="(img, index) in images" 
     v-show="index == cur_image" 
     :src='img'
-    :width="Cwidth">
-
-    <button class="btn" @click="after_image">></button>
+    :width="Cwidth"
+    height="170px">
+    <div class="btn left" @click="before_image"></div>
+    <div class="btn right" @click="after_image"></div>
   </div>
 </template>
 
@@ -35,19 +34,54 @@ module.exports = {
 
 <style>
 .carousel{
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  position: relative;
+}
+.carousel img{
+  z-index: 1;
 }
 .btn{
-  margin: 2px;
-  border-radius: 50%;
+  position: absolute;
+  height: 100%;
+  width: 20px;
+  margin: 0;
+  text-align: center;
+  background-color: rgba(128, 128, 128, 0.3);
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.btn:hover{
+  background-color: rgba(128, 128, 128, 0.6);
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
-  opacity: 0;
+.left{
+  z-index: 2;
+  left: 0px;
+  top: 0;
+  line-height: 0;
+}
+.right{
+  z-index: 2;
+  right: 0;
+  top: 0;
+  line-height: 0;
+}
+.left::after {
+  content: '';
+  position: absolute;
+  top:50%;
+  left: 0;
+  width: 0; 
+  height: 0; 
+  border-width: 0;
+  border: 10px solid transparent;
+  border-right: 10px solid white; 
+}
+.right:after {
+  content: '';
+  position: absolute;
+  top:50%;
+  right: 0;
+  width: 0; 
+  height: 0; 
+  border: 10px solid transparent;
+  border-left: 10px solid white; 
 }
 </style>
 
