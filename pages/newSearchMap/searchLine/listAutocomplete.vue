6458ffa692053bed0a7028962f9cdccf44abae10
git-SergBond <git-serg.bond@yandex.ru>
2018-09-03 19:04:19 +0300

    precomplete verstka avtokomplete

--- pages/newSearchMap/searchLine/listAutocomplete.vue
+++ pages/newSearchMap/searchLine/listAutocomplete.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
-        <div v-for="item in listData">
-            <p>тип: {{item.type}}, {{item.name}}, {{item.type=="point" ? item.address : "" }}  
-                <button @click="itemClick(item)">Посмотреть</button>
-            </p>
+    <div class="win-autocomplete">
+        <div class="item" v-for="item in listData" @click="itemClick(item)">
+            <img class="image" v-if="item.type == 'category'" src="images/icons/asd">
+            <img class="image" v-if="item.type == 'company'" src="images/icons/visitor.png">
+            <img class="image" v-if="item.type == 'service'" src="images/icons/ikonki-rabotaem-pod-klyuch_18_prev.png">
+            <p >{{item.name}}</p>
         </div>
     </div>
 </template>
@@ -30,6 +31,42 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
+<style scoped>
+.win-autocomplete{
+    width: 522px;
+    height: 300px;
+    margin-top: 10px;
+    overflow-y: scroll;
+    --btn-color: rgba(171,211,226,1);
+    background-color: white;
+    box-shadow: -1px -2px 20px -2px;
+}
+.win-autocomplete .item{
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: flex-start;
+}
+.win-autocomplete .item{
+    vertical-align: center;
+}
+.win-autocomplete .item:hover{
+    background-color: rgba(171,211,226,1)
+}
+.win-autocomplete .item .image{
+    width: 40px;
+    height: 40px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 2px;
+    margin-top: 2px;
+}
+.win-autocomplete::-webkit-scrollbar{
+    width: 20px;
+    padding: 3px;
+    background-color: transparent;
+}
+.win-autocomplete::-webkit-scrollbar-thumb{
+    background-color: var(--btn-color);
+}
 </style>
